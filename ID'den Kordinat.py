c1b32ae0ef54a1c003083084e3193934a04e6b7f
Volkan Savaş <114333088+lidlevolkan@users.noreply.github.com>
2022-11-20 18:07:22 +0300

    Add files via upload

---
+++ ID'den Kordinat.py
@@ -0,0 +1,29 @@
+sayi2 = 62
+
+ID1 = int(input("ID1 giriniz: "))
+
+X = ID1 * sayi2
+
+print("X:", ID1 * sayi2)
+
+X1 = str(X)
+
+ID2 = int(input("ID2 giriniz: "))
+
+Z = ID2 * sayi2
+
+print("Z:", ID2 * sayi2)
+
+Z1 = str(Z)
+
+mesaj = "/j world"
+Y = 65
+
+mesaj1 = str(mesaj)
+Y1 = str(Y)
+
+
+print("Kordinat:",X1 + "," + Y1 + "," + Z1)
+
+while True:
+    pass
---
+++ Kordinat'dan ID.py
@@ -0,0 +1,18 @@
+sayi2 = 62
+
+X = int(input("X giriniz: "))
+
+ID1 = X // sayi2
+
+print("ID1:", X // sayi2)
+
+Z = int(input("Z giriniz: "))
+
+ID2 = Z // sayi2
+
+print("ID2:", Z // sayi2)
+
+print("Arsa ID:", ID1,";",ID2)
+
+while True:
+    pass
