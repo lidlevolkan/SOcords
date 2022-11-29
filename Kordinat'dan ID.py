5a782af4f51457990010a8c55a8dcfb36e00e0dc
Volkan Savaş <114333088+lidlevolkan@users.noreply.github.com>
2022-11-30 00:31:52 +0300

    Update Kordinat'dan ID.py

--- Kordinat'dan ID.py
+++ Kordinat'dan ID.py
@@ -6,13 +6,18 @@ ID1 = X // sayi2
 
 print("ID1:", X // sayi2)
 
+X1 = str(ID1)
+
 Z = int(input("Z giriniz: "))
 
 ID2 = Z // sayi2
 
 print("ID2:", Z // sayi2)
 
-print("Arsa ID:", ID1,";",ID2)
+Z1 = str(ID2)
+
+print("Arsa ID:",X1 + ";" + Z1)
 
 while True:
     pass
+
